75ca3eb04ee8e19ed66da5467397b936086c6e78
knowmore <goooog@88.com>
2021-06-14 21:37:54 +0800

    feat: darcula colormap

--- config.h
+++ config.h
@@ -95,29 +95,30 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-    /* 8 normal colors */
-    [0] = "#000000", /* black   */
-    [1] = "#ff5555", /* red     */
-    [2] = "#50fa7b", /* green   */
-    [3] = "#f1fa8c", /* yellow  */
-    [4] = "#bd93f9", /* blue    */
-    [5] = "#ff79c6", /* magenta */
-    [6] = "#8be9fd", /* cyan    */
-    [7] = "#bbbbbb", /* white   */
 
-    /* 8 bright colors */
-    [8] = "#44475a",  /* black   */
-    [9] = "#ff5555",  /* red     */
-    [10] = "#50fa7b", /* green   */
-    [11] = "#f1fa8c", /* yellow  */
-    [12] = "#bd93f9", /* blue    */
-    [13] = "#ff79c6", /* magenta */
-    [14] = "#8be9fd", /* cyan    */
-    [15] = "#ffffff", /* white   */
+  /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#fa5355", /* red     */
+  [2] = "#67ff4f", /* green   */
+  [3] = "#ffff00", /* yellow  */
+  [4] = "#4581eb", /* blue    */
+  [5] = "#fa54ff", /* magenta */
+  [6] = "#33c2c1", /* cyan    */
+  [7] = "#ffffff", /* white   */
 
-    /* special colors */
-    [256] = "#282a36", /* background */
-    [257] = "#f8f8f2", /* foreground */
+  /* 8 bright colors */
+  [8]  = "#555555", /* black   */
+  [9]  = "#fb7172", /* red     */
+  [10] = "#67ff4f", /* green   */
+  [11] = "#ffff00", /* yellow  */
+  [12] = "#6d9df1", /* blue    */
+  [13] = "#fb82ff", /* magenta */
+  [14] = "#60d3d1", /* cyan    */
+  [15] = "#eeeeee", /* white   */
+
+  /* special colors */
+  [256] = "#2B2B2B", /* background */
+  [257] = "#fff9ff", /* foreground */
 };
 
 /*
